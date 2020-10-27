dac74c99d3a4f2d4f08b98cb820276df76f63a97
vanilla <rose20.99.c@gmail.com>
2020-10-27 14:32:14 +0800

    [update]

--- lib/test.d.ts
+++ lib/test.d.ts
@@ -1,9 +1,1 @@
-declare var Secure: any;
-declare let s: any;
-declare let uri1: any;
-declare let z: {
-    code: number;
-    crypto: string;
-    data: string;
-    msg: string;
-};
+export {};
--- test.js
+++ test.js
@@ -1,20 +1,18 @@
-
-
-
 // const { Secure } = new require("./secure")
+import Secure from "./lib/secure.d"
 
 // // cid 商户 id
 // // openid 三方用户 id
-// let s = new Secure("appxx", "1", "123456", "192006250b4c09247ec02edce69f6a2d")
+let s = new Secure("appxx", "1", "123456", "192006250b4c09247ec02edce69f6a2d", "1.0.1")
 
 // // 获取微信支付公共签名后的地址
-// let uri = s.getSign("https://mp.weixin.qq.com/wxamp/devprofile/get_profile?token=1515154505&lang=zh_CN")
+let uri = s.getSign("https://mp.weixin.qq.com/wxamp/devprofile/get_profile?token=1515154505&lang=zh_CN")
 // let uri2 = s.getSign("/get_profile?token=1515154505&lang=zh_CN")
 // let uri3 = s.getSign("www.baidu.com/get_profile?token=1515154505&lang=zh_CN")
 
 // let uri4 = s.getSignAppid("http://127.0.0.1:3223?token=1515154505&lang=zh_CN")
 
-// console.log(uri);
+console.log(uri);
 // console.log(uri2);
 // console.log(uri3);
 // console.log("uri4 => ", uri4);
